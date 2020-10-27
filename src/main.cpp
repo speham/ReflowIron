22caaf01def6995174c646dea355b5616b008886
vrees <viktor.rees@gmx.de>
2020-10-27 07:54:15 +0100

    added System out messages

--- src/main.cpp
+++ src/main.cpp
@@ -29,24 +29,29 @@ int offset = 0;
 long t = millis();
 long t_solder = millis();
 
-
-int X(int textgroesse, int n) {
-  return (0.5 * (display.width() - textgroesse * (6 * n - 1)));  //end int X
+int X(int textgroesse, int n)
+{
+  return (0.5 * (display.width() - textgroesse * (6 * n - 1))); //end int X
 }
-int Y(int textgroesse, float f) {
-  return (f * display.height() - (textgroesse * 4));  //end int Y
+int Y(int textgroesse, float f)
+{
+  return (f * display.height() - (textgroesse * 4)); //end int Y
 }
 
-void regulate_temp(int temp, int should) {
-  if (should <= temp - offset) {
+void regulate_temp(int temp, int should)
+{
+  if (should <= temp - offset)
+  {
     digitalWrite(solidstate, LOW);
   }
-  else if (should > temp + offset) {
+  else if (should > temp + offset)
+  {
     digitalWrite(solidstate, HIGH);
   }
 }
 
-void PrintScreen(String state, int soll_temp, int ist_temp, int tim, int percentage) {
+void PrintScreen(String state, int soll_temp, int ist_temp, int tim, int percentage)
+{
   display.clearDisplay();
   display.setTextColor(WHITE);
   display.setTextSize(1);
@@ -56,12 +61,14 @@ void PrintScreen(String state, int soll_temp, int ist_temp, int tim, int percent
   String str = String(soll_temp) + " deg";
   display.println(str);
 
-  if (tim != 0) {
+  if (tim != 0)
+  {
     display.setCursor(0, 50);
     str = String(tim) + " sec";
     display.println(str);
   }
-  if (percentage != 0) {
+  if (percentage != 0)
+  {
     display.setCursor(80, 50);
     str = String(percentage) + " %";
     display.println(str);
@@ -73,29 +80,57 @@ void PrintScreen(String state, int soll_temp, int ist_temp, int tim, int percent
   display.display();
 }
 
-void setup() {
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("setup: will init ports");
+
   pinMode(button, INPUT);
   pinMode(solidstate, OUTPUT);
   digitalWrite(solidstate, LOW);
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+
+  Serial.println("setup: will init display now ...");
+  display.begin();
+  //display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+
+  Serial.println("setup: will clear display now ...");
   display.clearDisplay();
   display.display();
+
+  display.fillScreen(WHITE);
+  display.setTextSize(1);
+  display.setCursor(X(1, 6), Y(1, 0.1));
+  display.println("REFLOW");
+
+  delay(3000);
+  Serial.println("setup: display done");
 }
 
-void loop() {
+void loop()
+{
   temp_now = thermocouple.readCelsius();
-  temp_poti = map(analogRead(poti), 1023, 0, temp_preheat, temp_reflow);
+  Serial.print("temp_now=");
+  Serial.println(temp_now);
 
-  if (temp_poti != temp_poti_old) {
+  temp_poti = map(analogRead(poti), 1023, 0, temp_preheat, temp_reflow);
+  Serial.print("temp_poti=");
+  Serial.println(temp_poti);
+
+
+
+  if (temp_poti != temp_poti_old)
+  {
     long v = millis();
     display.fillScreen(WHITE);
     display.setTextSize(1);
     display.setCursor(X(1, 6), Y(1, 0.1));
     display.println("REFLOW");
     display.setTextSize(2);
-    while (millis() < v + 2000) {
+    while (millis() < v + 2000)
+    {
       temp_poti = map(analogRead(poti), 1023, 0, temp_preheat, temp_reflow);
-      if (temp_poti > temp_poti_old + 1 || temp_poti < temp_poti_old - 1) {
+      if (temp_poti > temp_poti_old + 1 || temp_poti < temp_poti_old - 1)
+      {
         display.setCursor(X(2, 3), Y(2, 0.5));
         display.println(String(temp_poti));
         display.display();
@@ -106,16 +141,20 @@ void loop() {
     temp_poti_old = temp_poti;
   }
 
-  if (millis() > t + 200 || millis() < t) {
-    PrintScreen(state[state_now], temp_next, temp_now, time_count, perc);
+  if (millis() > t + 200 || millis() < t)
+  {
+    // PrintScreen(state[state_now], temp_next, temp_now, time_count, perc);
     t = millis();
   }
 
-  if (digitalRead(button) == 0) {
+  if (digitalRead(button) == 0)
+  {
     delay(100);
     long c = millis();
-    while (digitalRead(button) == 0) {
-      if (millis() > c + 1500) {
+    while (digitalRead(button) == 0)
+    {
+      if (millis() > c + 1500)
+      {
         digitalWrite(solidstate, LOW);
         state_now = 0;
         display.fillScreen(WHITE);
@@ -124,7 +163,8 @@ void loop() {
         display.setCursor(X(2, 3), Y(2, 0.5));
         display.println("OFF");
         display.display();
-        while (digitalRead(button) == 0) delay(1);
+        while (digitalRead(button) == 0)
+          delay(1);
         return;
       }
     }
@@ -132,38 +172,49 @@ void loop() {
     t_solder = millis();
     perc = 0,
     state_now++;
-    if (state_now == 0) temp_next = 0;
-    else if (state_now == 1) temp_next = temp_preheat;
-    else if (state_now == 2) temp_next = temp_poti;
-    else if (state_now == 3) temp_next = 0;
-    else if (state_now == 4) {
+    if (state_now == 0)
+      temp_next = 0;
+    else if (state_now == 1)
+      temp_next = temp_preheat;
+    else if (state_now == 2)
+      temp_next = temp_poti;
+    else if (state_now == 3)
+      temp_next = 0;
+    else if (state_now == 4)
+    {
       state_now = 0;
       temp_next = 0;
     }
   }
 
-  if (state_now == 1) { //PREHEAT
+  if (state_now == 1)
+  { //PREHEAT
     regulate_temp(temp_now, temp_next);
     perc = int((float(temp_now) / float(temp_next)) * 100.00);
   }
-  else if (state_now == 2) { //REFLOW
+  else if (state_now == 2)
+  { //REFLOW
     regulate_temp(temp_now, temp_next);
     perc = int((float(temp_now) / float(temp_next)) * 100.00);
-    if (perc >= 100) {
+    if (perc >= 100)
+    {
       state_now = 3;
       t_solder = millis();
       perc = 0;
       temp_next = 0;
     }
   }
-  else if (state_now == 3) { //COOLING
+  else if (state_now == 3)
+  { //COOLING
     digitalWrite(solidstate, LOW);
     time_count = int((t_solder + 60000 - millis()) / 1000);
-    if (time_count <= 0) {
+    if (time_count <= 0)
+    {
       state_now = 0;
     }
   }
-  else {
+  else
+  {
     digitalWrite(solidstate, LOW);
     time_count = 0;
   }
